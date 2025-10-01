2c9509285844b2ba83bce419d98490828f9cf9d4
Sebastian <andersen_sebastian@yahoo.dk>
2025-10-01 19:06:21 +0200

    Add player movement and add player collision

--- efterår 2025/pong_progress.cs
+++ efterår 2025/pong_progress.cs
@@ -6,6 +6,11 @@ Raylib.SetTargetFPS(60);
 
 var position = new Vector2(100,100);
 var direction = new Vector2(1, 1);
+
+var playerPosition = new Vector2(20, 200);
+var playerSize = new Vector2(20, 150);
+var playerSpeed = 10;
+
 var speed = 3;
 
 var radius = 50;
@@ -21,12 +26,23 @@ while (!Raylib.WindowShouldClose())
         direction.Y *= -1;
     }
 
-    if(position.X + radius >= Raylib.GetScreenWidth()
-        || position.X - radius <= 0)
+    if(position.X + radius >= Raylib.GetScreenWidth())
     {
         direction.X *= -1;
     }
 
+    if (position.X - radius <= 0)
+    {
+        Console.WriteLine("død");
+    }
+
+    MovePlayer();
+
+    if (Raylib.CheckCollisionCircleRec(position, radius, new Rectangle(playerPosition, playerSize)))
+    {
+        direction.X *= -1;
+    }
+    
     Console.WriteLine(position);
 
     // render
@@ -35,12 +51,22 @@ while (!Raylib.WindowShouldClose())
 
     Raylib.DrawFPS(0, 0);
 
-    var playerPosition = new Vector2(20, 200);
-    var playerSize = new Vector2(20, 150);
-
     Raylib.DrawRectangleV(playerPosition, playerSize, Color.Pink);
 
     Raylib.DrawCircleV(position, radius, Color.Red);
 
     Raylib.EndDrawing();
+}
+
+void MovePlayer()
+{
+    if (Raylib.IsKeyDown(KeyboardKey.S))
+    {
+        playerPosition.Y = Raymath.Clamp(playerPosition.Y + playerSpeed, 0, Raylib.GetScreenHeight() - playerSize.Y);
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.W))
+    {
+        playerPosition.Y = Raymath.Clamp(playerPosition.Y - playerSpeed, 0, Raylib.GetScreenHeight() - playerSize.Y);
+    }
 }
