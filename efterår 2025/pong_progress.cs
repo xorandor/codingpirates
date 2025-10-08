a9befcd70d5a1f5ec8684595f94b5e677d82d231
xorandor <allan@loopie.dk>
2025-10-08 19:01:59 +0200

    Update pong_progress.cs

--- efterår 2025/pong_progress.cs
+++ efterår 2025/pong_progress.cs
@@ -1,32 +1,37 @@
+
 using Raylib_cs;
 using System.Numerics;
 
 Raylib.InitWindow(800, 600, "Pong Game");
 Raylib.SetTargetFPS(60);
 
-var position = new Vector2(100,100);
+var position = new Vector2(100, 100);
 var direction = new Vector2(1, 1);
 
 var playerPosition = new Vector2(20, 200);
 var playerSize = new Vector2(20, 150);
 var playerSpeed = 10;
 
-var speed = 3;
+var enemyPosition = new Vector2(Raylib.GetScreenWidth() - 40, 200);
 
-var radius = 50;
+var speed = 5;
+
+var radius = 20;
 
 while (!Raylib.WindowShouldClose())
 {
-    //update state
+    MovePlayer();
+    MoveEnemy();
+
     position += direction * speed;
 
-    if((position.Y + radius) >= Raylib.GetScreenHeight()
+    if ((position.Y + radius) >= Raylib.GetScreenHeight()
         || position.Y - radius <= 0)
     {
         direction.Y *= -1;
     }
 
-    if(position.X + radius >= Raylib.GetScreenWidth())
+    if (position.X + radius >= Raylib.GetScreenWidth())
     {
         direction.X *= -1;
     }
@@ -36,13 +41,17 @@ while (!Raylib.WindowShouldClose())
         Console.WriteLine("død");
     }
 
-    MovePlayer();
 
     if (Raylib.CheckCollisionCircleRec(position, radius, new Rectangle(playerPosition, playerSize)))
     {
         direction.X *= -1;
     }
-    
+
+    if (Raylib.CheckCollisionCircleRec(position, radius, new Rectangle(enemyPosition, playerSize)))
+    {
+        direction.X *= -1;
+    }
+
     Console.WriteLine(position);
 
     // render
@@ -51,7 +60,22 @@ while (!Raylib.WindowShouldClose())
 
     Raylib.DrawFPS(0, 0);
 
+    var lineHeight = 0;
+    var lineSize = 20;
+
+    while(lineHeight < Raylib.GetScreenHeight())
+    {
+        Raylib.DrawLineEx(
+            new Vector2(Raylib.GetScreenWidth() / 2, lineHeight), 
+            new Vector2(Raylib.GetScreenWidth() / 2, lineHeight + lineSize), 3, Color.Black);
+
+
+        lineHeight += lineSize + 7;
+    }
+
+
     Raylib.DrawRectangleV(playerPosition, playerSize, Color.Pink);
+    Raylib.DrawRectangleV(enemyPosition, playerSize, Color.Red);
 
     Raylib.DrawCircleV(position, radius, Color.Red);
 
@@ -68,5 +92,18 @@ void MovePlayer()
     if (Raylib.IsKeyDown(KeyboardKey.W))
     {
         playerPosition.Y = Raymath.Clamp(playerPosition.Y - playerSpeed, 0, Raylib.GetScreenHeight() - playerSize.Y);
+    }
+}
+
+void MoveEnemy()
+{
+    if (position.Y > enemyPosition.Y + playerSize.Y / 2)
+    {
+        enemyPosition.Y = Raymath.Clamp(enemyPosition.Y + playerSpeed, 0, Raylib.GetScreenHeight() - playerSize.Y);
+    }
+
+    if (position.Y < enemyPosition.Y + playerSize.Y / 2)
+    {
+        enemyPosition.Y = Raymath.Clamp(enemyPosition.Y - playerSpeed, 0, Raylib.GetScreenHeight() - playerSize.Y);
     }
 }
