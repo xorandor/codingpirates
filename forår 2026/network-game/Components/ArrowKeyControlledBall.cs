22301cd37a221756aa031f71a2036172e543ac5a
Sebastian <andersen_sebastian@yahoo.dk>
2026-03-18 18:29:45 +0100

    Revert "Update ArrowKeyControlledBall.cs"
    This reverts commit 1000a5dcf3cde3f0ec90461a00d100a692f17ba8.

--- forår 2026/network-game/Components/ArrowKeyControlledBall.cs
+++ forår 2026/network-game/Components/ArrowKeyControlledBall.cs
@@ -16,8 +16,7 @@ public class ArrowKeyControlledBall : IComponent
     private readonly bool _isOwn;     // client only: true = controlled by this player
     public string Name { get; }
     private readonly bool _showName;
-    private bool _movedThisFrame;
-        
+
     public ArrowKeyControlledBall(Vector2 position, float speed, float radius, Color color, int ballId = -1, bool isOwn = false, string name = "", bool showName = false)
     {
         Position = position;
@@ -52,16 +51,10 @@ public class ArrowKeyControlledBall : IComponent
         else if (_isOwn)
         {
             MoveWithArrowKeys();
-
-            if (_movedThisFrame)
-            {
-                context.Networking.SendMessageToServer("BOXMOVE",
-                    _ballId.ToString(),
-                    ((int)Position.X).ToString(),
-                    ((int)Position.Y).ToString());
-                
-                _movedThisFrame = false;
-            }
+            context.Networking.SendMessageToServer("BOXMOVE",
+                _ballId.ToString(),
+                ((int)Position.X).ToString(),
+                ((int)Position.Y).ToString());
         }
         else
         {
@@ -86,7 +79,6 @@ public class ArrowKeyControlledBall : IComponent
         {
             direction = Vector2.Normalize(direction);
             Position += direction * _speed * GetFrameTime();
-            _movedThisFrame = true;
         }
     }
 
